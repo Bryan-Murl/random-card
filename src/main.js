204b88f3b281e9ece3664d3080d599abe5e6e180
Bryan-Murl <murillo.bryan@outlook.com>
2021-02-16 21:35:48 +0000

    random card generator day10

--- src/main.js
+++ src/main.js
@@ -6,5 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbol = ["♦", "♣", "♥", "♠"];
+
+  let z = Math.floor(Math.random() * symbol.length);
+  let topx = symbol[z];
+  let centerx = number[Math.floor(Math.random() * number.length)];
+  let bottomx = symbol[z];
+  document.querySelector("#symbol-top").innerHTML = topx;
+  document.querySelector("#card-number").innerHTML = centerx;
+  document.querySelector("#symbol-bottom").innerHTML = bottomx;
 };
