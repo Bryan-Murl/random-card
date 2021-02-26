4e92cf292f4e24052a6dbbbaa0d10db356d25605
Bryan-Murl <murillo.bryan@outlook.com>
2021-02-26 02:12:34 +0000

    cardd generator

--- src/main.js
+++ src/main.js
@@ -21,13 +21,36 @@ window.onload = function() {
     "Q",
     "K"
   ];
-  let symbol = ["♦", "♣", "♥", "♠"];
+  let symbol = ["\u2666", "\u2660", "\u2665", "\u2663"];
 
+  //  Simbolo y número aletorio
   let z = Math.floor(Math.random() * symbol.length);
   let topx = symbol[z];
-  let centerx = number[Math.floor(Math.random() * number.length)];
   let bottomx = symbol[z];
+  let centerx = number[Math.floor(Math.random() * number.length)];
+
   document.querySelector("#symbol-top").innerHTML = topx;
   document.querySelector("#card-number").innerHTML = centerx;
   document.querySelector("#symbol-bottom").innerHTML = bottomx;
+
+  // Asignar color
+  document.querySelector("#symbol-top").style.color = colors(topx);
+  document.querySelector("#symbol-bottom").style.color = colors(bottomx);
+  //   document.querySelector("#card-number").style.color = colorNo(centerx);
 };
+
+function colors(palos) {
+  if (palos == "\u2665" || palos == "\u2666") {
+    return "red";
+  } else {
+    return "black";
+  }
+}
+
+// function colorNo(palo) {
+//   if (palo == "\u2665" || palo == "\u2666") {
+//     return "black";
+//   } else {
+//     return "red";
+//   }
+// }
